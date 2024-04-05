48912038b2042e3464af97f2b420b9563908b577
MintBlue06 <p1210mizu@gmail.com>
2024-04-05 15:34:20 -0300

    feat: Agrega punto1 del TP1 de Programacion Visual

---
+++ TP1/src/ar/edu/unju/fi/punto1/Punto1.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.punto1;
+
+public class Punto1 {
+	public static void main(String[] args) {
+		
+	int numero1 = 5;
+	int numero2 = 7;
+	int numero3 = 3;
+	int numero4 = 4;
+	int numero5 = 9;
+	
+	int suma = numero1 + numero2 + numero3 + numero4 + numero5;
+    
+    float resultado = suma / 5;
+    //* Resultado de la operacion */
+    System.out.println("Resultado del promedio = " + resultado);
+	}
+}
