9b089ad8b73e1ab01d3c4eca346ab0bfc924b8ca
MintBlue06 <p1210mizu@gmail.com>
2024-04-13 23:58:37 -0300

    feta: Agrega el punto3, y el punto enviado anteriormente era el 5 no el 6

--- TP1/src/ar/edu/unju/fi/punto3/Main.java
+++ TP1/src/ar/edu/unju/fi/punto3/Main.java
@@ -1,10 +1,34 @@
 package ar.edu.unju.fi.punto3;
+//ingresa un numero y lo multiplica por 2 si es par
+//y por 3 si no lo es
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Main main = new Main();
+		//main.ingresarNumero();
+		main.multiplicarNumero();
 	}
+	public int ingresarNumero() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero entero");
+        int numero = scanner.nextInt();
+        return numero;
+        //scanner.close();
+    }
+	public void multiplicarNumero() {
+		int producto, numero=ingresarNumero();
+        boolean par = (numero % 2) == 0;
 
+        if(par) {
+            System.out.println("El numero " + numero + " es par");
+            producto = numero*3;
+            System.out.println(numero + "*3 = " + producto);
+        } else {
+        	System.out.println("El numero " + numero + " es impar");
+            producto = numero*2;
+            System.out.println(numero + "*2 = " + producto);
+        	}
+    }
 }
